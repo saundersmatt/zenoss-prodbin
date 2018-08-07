3a1ae2aea09324db8c27dca29f23bd189c15f01e
lundybernard <lundy.bernard@gmail.com>
2018-08-07 14:39:12 -0500

    update registerDirectory method for ZenUtils

--- Products/ZenUtils/__init__.py
+++ Products/ZenUtils/__init__.py
@@ -7,8 +7,6 @@
 #
 ##############################################################################
 
-from Products.CMFCore.DirectoryView import registerDirectory
-registerDirectory('js', globals())
 
 # import any monkey patches that may be necessary
 from patches import pasmonkey
