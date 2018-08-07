686f7cc1d2330db7dceda5c0db16595e0925cd5b
lundybernard <lundy.bernard@gmail.com>
2018-08-07 14:37:51 -0500

    update registerDirectory method for ZenModel

--- Products/ZenModel/__init__.py
+++ Products/ZenModel/__init__.py
@@ -1,10 +1,10 @@
 ##############################################################################
-# 
+#
 # Copyright (C) Zenoss, Inc. 2007, all rights reserved.
-# 
+#
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
-# 
+#
 ##############################################################################
 
 
@@ -18,7 +18,7 @@ the file must be the same.
 
 If there is a ZMI add screen it must be called "add" + class name (ie addDevice)and it must be defined at the module level.
 
-the class factory must be a function at the module level called 
+the class factory must be a function at the module level called
 manage_add + class name (ie manage_addDevice)
 
 If there is an icon for the product it should be called class name + _icon.gif
@@ -30,8 +30,6 @@ log = logging.getLogger("zenmodel")
 
 if 0:
     __path__ = None                     # keep pyflakes quiet
-
-from Products.CMFCore.DirectoryView import registerDirectory
 
 confmon_globals = globals()
 
@@ -176,11 +174,6 @@ productNames = (
     'ZenossSecurity',
     'ZentinelPortal',
 )
-
-# Make the skins available as DirectoryViews.
-registerDirectory('skins', globals())
-registerDirectory('help', globals())
-
 
 confmonModules = []
 def loadConfmonModules():
